dc4f93353cbda1b7d2358782ade4a979ea866e68
Victor Evangelista <victor.evangelista@accelera.com.br>
2024-08-17 13:06:52 -0300

    att vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/projeto3d/',
+  publicPath: '/site-sobre-mim/',
   transpileDependencies: true
 })
