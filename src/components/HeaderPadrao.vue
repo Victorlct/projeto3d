b86400f5ecf15450c0e9739cd92d7d4dea7d086a
Victor Evangelista <victor.evangelista@accelera.com.br>
2024-08-17 12:47:34 -0300

    add links no header

--- src/components/HeaderPadrao.vue
+++ src/components/HeaderPadrao.vue
@@ -3,23 +3,23 @@
     <nav class="nav px-3">
       <ul>
         <li>
-          <a href="#home">
-            <img src="/linkedin.png" alt="LinkedIn" width="32px" height="32px">
+          <a href="https://www.linkedin.com/in/victor-la-corte-348b81250/" target="_blank">
+            <img src="linkedin.png" alt="LinkedIn" width="32px" height="32px">
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Victorlct" target="_blank">
+            <img src="github.png" alt="Github" width="32px" height="32px">
+          </a>
+        </li>
+        <li>
+          <a href="mailto:victorlacortee@gmail.com">
+            <img src="gmail.png" alt="Gmail" width="32px" height="32px">
           </a>
         </li>
         <li>
           <a href="#home">
-            <img src="/github.png" alt="Github" width="32px" height="32px">
-          </a>
-        </li>
-        <li>
-          <a href="#home">
-            <img src="/gmail.png" alt="Gmail" width="32px" height="32px">
-          </a>
-        </li>
-        <li>
-          <a href="#home">
-            <img src="/leetcode.png" alt="Leetcode" width="32px" height="32px">
+            <img src="leetcode.png" alt="Leetcode" width="32px" height="32px">
           </a>
         </li>
       </ul>
